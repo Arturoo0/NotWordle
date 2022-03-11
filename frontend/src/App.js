15bcfc939e61735b5604e958796faa3917ddb1af
Arturoo0 <41554978+Arturoo0@users.noreply.github.com>
2022-03-11 14:03:25 -0500

    Add basic routing to the App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Auth from './pages/Auth.js';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Auth />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
