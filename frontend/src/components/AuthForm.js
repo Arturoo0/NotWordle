9fc61c0d3f470629ca877596fb2b183d32b00524
Arturoo0 <41554978+Arturoo0@users.noreply.github.com>
2022-03-10 23:24:25 -0500

    Create inputs with their respective update funcs

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
 
 const AuthForm = () => {
+    const [ email, setEmail ] = useState(null);
+    const [ username, setUsername ] = useState(null);
+    const [ password, setPassword ] = useState(null);
+
+    const handleUserSumbmission = () => {
+        return null;
+    };
+
+    const renderFormInputs = () => {
+        const inputTypes = [
+            { 'credential' : 'Email', 'update' : (userCredential) => { setEmail(userCredential)}},
+            { 'credential' : 'Username', 'update' : (userCredential) => { setUsername(userCredential)}},
+            { 'credential' : 'Password', 'update' : (userCredential) => { setPassword(userCredential)}}   
+        ];
+        const inputs = inputTypes.map((inputType) =>
+            <div>
+                <p>{inputType.credential}</p>
+                <input onChange={(event) => inputType.update(event.target.value) }></input>
+            </div>
+        );
+        return inputs;
+    };
+
     return (
         <div>
-            Auth form
+            {renderFormInputs()}
+            <button onClick={() => { handleUserSumbmission() }}>Submit</button>
         </div>
     )
 };
