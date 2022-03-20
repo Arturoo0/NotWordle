30f4fee77695875714d9c91b79358d2372cb5b4c
Arturoo0 <41554978+Arturoo0@users.noreply.github.com>
2022-03-19 23:05:44 -0400

    Allow for free play navigation

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { AuthForm } from '../components';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Auth = () => {
+    const navigate = useNavigate();
     const AuthPageContainer = {
         height: '100vh',
         width: '100vw',
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
-        backgroundColor: '#112D4E'
+        backgroundColor: '#112D4E',
+        flexDirection: 'column'
     };
 
     return (
         <div style={AuthPageContainer}> 
             <AuthForm />
+            <Button
+                onClick={(event) => {
+                    event.preventDefault();
+                    navigate('free-play');
+                }}
+            variant='outline-primary'>Continue without an account</Button>
         </div>
     );
 };
