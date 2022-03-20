97f5ae60b8a579082d54ba419f5c2ff119ac18e6
Arturoo0 <41554978+Arturoo0@users.noreply.github.com>
2022-03-19 23:45:35 -0400

    Add port check in app master

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const appCore = async () => {
 const startApp = async () => {
   await appCore();
 
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   const app = express();
 
   app.use(cors({
