e5754b6a0b80fa632905c88f70dbf348c101666a
Arturoo0 <41554978+Arturoo0@users.noreply.github.com>
2022-03-20 00:04:28 -0400

    Remove extra / in req address

--- frontend/src/utils/baseRequest.js
+++ frontend/src/utils/baseRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-const baseReqPath = 'https://api-not-wordle.herokuapp.com/';
+const baseReqPath = 'https://api-not-wordle.herokuapp.com';
 
 const checkPassedEndpoint = (endpoint) => {
     if (endpoint === undefined){
