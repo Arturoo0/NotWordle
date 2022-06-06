d1ba8097c70bb05663a8231fed80d5e98caa54a7
Arturoo0 <41554978+Arturoo0@users.noreply.github.com>
2022-06-06 15:25:08 -0400

    Check structure of incoming post game info

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const joi = require('joi');
 
+const { User } = require('../models/User.js');
 const userRouter = express.Router();
 
+const userGameSchema = joi.object({
+    isWinner: joi.boolean()
+        .required()
+})
+
 userRouter.post('/save-game-result', async (req, res) => {
-    console.log(req.body);
+    const gameInfo = req.body;
+    console.log(userGameSchema.validate(gameInfo));
 });
 
 module.exports = userRouter;
