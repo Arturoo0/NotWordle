a2ddea7720555648b4517c119253b4c546213959
Arturoo0 <41554978+Arturoo0@users.noreply.github.com>
2022-06-15 17:26:59 -0400

    Pull session email info in seperate function

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,24 +15,30 @@ const userGameSchema = joi.object({
         .required()
 })
 
+const pullSessionEmail = async (req) => {
+    const session = await Session.findOne({
+        sessionIdentifier: req.headers.sessiontokenid
+    });
+    return session;
+}
+ 
 userRouter.post('/save-game-result', async (req, res) => {
     const gameInfo = req.body;
     const { error } = userGameSchema.validate(gameInfo);
     if (!error){
-        const session = await Session.findOne({
-            sessionIdentifier: gameInfo.sessionTokenId
-        });
-        if (session){
-            const user = await User.findOne({email: session.associatedSessionEmail});
-            user.games.push(gameInfo);
-            user.save();
-        }        
+        const session = await pullSessionEmail(req);
+        const user = await User.findOne({email: session.associatedSessionEmail});
+        user.games.push(gameInfo);
+        user.save();
+           
     }
 });
 
 userRouter.get('/fetch-history', async (req, res) => {
+    const session = await pullSessionEmail(req);
+    const user = await User.findOne({email: session.associatedSessionEmail});
     return res.send({
-        history: []
+        history: user.games
     });
 }); 
 
